ee2fcc75950d7849f7a2cf14dadba2f328666396
Justin Chen <ch3njus@gmail.com>
2023-05-29 18:18:34 -0400

    feat: add openai api

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dynamic Meta-programming with Large Language Models
 Metaprogramming with safe code injection from [generative APIs](https://github.com/ch3njust1n/generative-api) for adaptable software
+
+# Setup instructions
+1. Create `.env` for your environment variables
+2. Add your API key
--- meta.py
+++ meta.py
@@ -4,24 +4,35 @@ from typing import Callable, Any
 A decorator that replaces the behavior of the decorated function with arbitrary code.
 
 Args:
-    code: A string of Python code that returns a result.
+    code    (string): A string of Python code that returns a result.
+    use_llm (boolean): True to use a LLM to generate code, False otherwise.
 
 Returns:
     A function that wraps the original function, replacing its behavior with the provided code.
 """
 
-
+import os
 import inspect
 from typing import Callable, Any, Optional
 
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def adapt(code: str) -> Callable:
+def adapt(code: str, use_llm: bool = False) -> Callable:
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        nonlocal code
         func_source: Optional[str] = None
         try:
             # Get the source code of the function
             func_source = inspect.getsource(func)
-            print("\nsource\n" + func_source)
+            if use_llm:
+                llm_code = openai.Completion.create(
+                    model=os.getenv("MODEL"),
+                    prompt=f"source\n{func_source}",
+                    temperature=os.getenv("TEMPERATURE"),
+                )
+                code = llm_code.choices[0].text
+            
         except (TypeError, OSError):
             # Handle the error if the source code could not be retrieved
             print("Could not retrieve the source code of the function.")
@@ -36,7 +47,10 @@ def adapt(code: str) -> Callable:
                     "kwargs": kwargs,
                     "func_source": func_source,  # make the source code available to the new code
                 }
+                
+                # TODO: santize given function using traditional methods and LLM
                 exec(f"result = {code}", local_vars)
+                # TODO: sanitize result
                 return local_vars.get("result")
 
         return wrapper
