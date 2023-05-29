03f7d4c2dc34cfa2afe27115088ff4c1da3cc782
Justin Chen <ch3njus@gmail.com>
2023-05-29 17:38:24 -0400

    style: black

--- meta.py
+++ meta.py
@@ -14,17 +14,18 @@ Returns:
 import inspect
 from typing import Callable, Any, Optional
 
+
 def adapt(code: str) -> Callable:
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
         func_source: Optional[str] = None
         try:
             # Get the source code of the function
             func_source = inspect.getsource(func)
-            print("\nsource\n"+func_source)
+            print("\nsource\n" + func_source)
         except (TypeError, OSError):
             # Handle the error if the source code could not be retrieved
             print("Could not retrieve the source code of the function.")
-        
+
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             if code == "":
                 # If no code is provided, run the decorated function as normal
@@ -41,4 +42,3 @@ def adapt(code: str) -> Callable:
         return wrapper
 
     return decorator
-
