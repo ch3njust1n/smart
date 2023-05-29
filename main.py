2bf5fabe136236577b48c5abdcbc0a3afd9967d9
Justin Chen <ch3njus@gmail.com>
2023-05-29 16:35:36 -0400

    chore: add comments, style black, and add type hinting

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+# Demo code for dynamic metaprogramming decorator
+
 from meta import adapt
 
 a = "sum(args)"
 
+
 @adapt(a)
 def add(a, b):
-	return a + b
+    return a + b
+
 
 print(add(3, 4))  # Prints: 7
 
 b = ""
 
+
 @adapt(b)
 def multiply(a, b):
-	return a * b
+    return a * b
+
 
 print(multiply(3, 4))  # Prints: 12
--- meta.py
+++ meta.py
@@ -1,17 +1,28 @@
-# File: decorators.py
+from typing import Callable, Any
 
-def adapt(code):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
+"""
+A decorator that replaces the behavior of the decorated function with arbitrary code.
+
+Args:
+    code: A string of Python code that returns a result.
+
+Returns:
+    A function that wraps the original function, replacing its behavior with the provided code.
+"""
+def adapt(code: str) -> Callable:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             if code == "":
                 # If no code is provided, run the decorated function as normal
                 return func(*args, **kwargs)
             else:
                 local_vars = {
-                    'args': args,
-                    'kwargs': kwargs,
+                    "args": args,
+                    "kwargs": kwargs,
                 }
                 exec(f"result = {code}", local_vars)
-                return local_vars.get('result')
+                return local_vars.get("result")
+
         return wrapper
+
     return decorator
