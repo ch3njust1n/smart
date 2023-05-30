5c7e79394d80a3256e1952fe02bbf424fd4783d3
Justin Chen <ch3njus@gmail.com>
2023-05-29 22:57:33 -0400

    feat: add prompt to LLM test

--- tests/test_adapt_decorator.py
+++ tests/test_adapt_decorator.py
@@ -49,12 +49,27 @@ def test_self_healing():
     assert broken_fibonacci(8) == 21
 
 
-# def test_llm():
-#     @adapt(use_llm=True)
-#     def func(a, b):
-#         prompt="""
-#         Write a complete python 3 function, including the header and
-#         return statement that computes the N-th Fibonacci number.
-#         """
+def test_llm():
+    @adapt(use_llm=True)
+    def func(a, b):
+        prompt="""
+        Write a complete python 3 function, including the header and
+        return statement that computes the N-th Fibonacci number.
+        Do not write a driver program, do not comment, do not explain. 
+        Do not write any code outside of the function body.
+        Do not call the function or return a reference to it.
+        For example, only do this:
+        ```
+        def func():
+        # function body
+        ```
+        and do not do this:
+        ```
+        def func():
+        # function body
+        return func()
+        ```
+        Only return the function header and body!
+        """
 
-#     assert func(8) == 21
+    assert func(8) == 21
