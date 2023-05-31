0b41f8308b1aed8012ab030640bb6ad2eacdb36b
Justin Chen <ch3njus@gmail.com>
2023-05-31 10:30:58 -0400

    feat: update tests to also use claude

--- tests/test_adapt_decorator.py
+++ tests/test_adapt_decorator.py
@@ -46,14 +46,14 @@ def test_self_healing_no_llm():
     assert broken_fibonacci(8) == 21
 
 
-def test_self_healing_with_llm():
+def test_self_healing_with_gpt():
     retry_count = 3
     retry_delay = 5
 
     for _ in range(retry_count):
         try:
 
-            @adapt(llm=llm)
+            @adapt(llm=gpt)
             def fibonacci(n):
                 if n <= 0:
                     return 0
@@ -69,7 +69,30 @@ def test_self_healing_with_llm():
             time.sleep(retry_delay)
 
 
-def test_llm():
+def test_self_healing_with_claude():
+    retry_count = 3
+    retry_delay = 5
+
+    for _ in range(retry_count):
+        try:
+
+            @adapt(llm=claude)
+            def fibonacci(n):
+                if n <= 0:
+                    return 0
+                elif n == 1:
+                    return 1
+                else:
+                    return fibonacci(n - 1) + fibonacci(n - 2) + 1
+
+            assert fibonacci(8) == 21
+            break
+        except Exception as e:
+            print(f"Test error: {e}. Retrying after delay...")
+            time.sleep(retry_delay)
+
+
+def test_adapt_with_gpt():
     retry_count = 3
     retry_delay = 5
 
@@ -90,7 +113,28 @@ def test_llm():
             time.sleep(retry_delay)
 
 
-def test_catch():
+def test_adapt_with_claude():
+    retry_count = 3
+    retry_delay = 5
+
+    for _ in range(retry_count):
+        try:
+
+            @adapt(llm=claude)
+            def func(a, b):
+                prompt = """
+                Write a complete python 3 function, including the header and
+                return statement that computes the N-th Fibonacci number.
+                """
+
+            assert func(8) == 21
+            break
+        except Exception as e:
+            print(f"Test error: {e}. Retrying after delay...")
+            time.sleep(retry_delay)
+
+
+def test_catch_with_gpt():
     retry_count = 3
     retry_delay = 5
 
@@ -110,7 +154,27 @@ def test_catch():
             time.sleep(retry_delay)
 
 
-def test_stack_trace():
+def test_catch_with_claude():
+    retry_count = 3
+    retry_delay = 5
+
+    for _ in range(retry_count):
+        try:
+            # Define a function that raises an exception
+            @catch(llm=claude)
+            def func(a, b):
+                raise Exception("Original function exception")
+
+            # Since the original function raises an exception, we expect the LLM
+            # to provide an implementation that returns the sum of a and b
+            assert func(3, 4) == 7
+            break
+        except Exception as e:
+            print(f"Test error: {e}. Retrying after delay...")
+            time.sleep(retry_delay)
+
+
+def test_stack_trace_with_gpt():
     retry_count = 3
     retry_delay = 5
 
@@ -118,6 +182,29 @@ def test_stack_trace():
         try:
 
             @stack_trace(llm=gpt)
+            def funkodunko():
+                items = [1, 2, 3]
+                return items[5]
+
+            try:
+                funkodunko()
+            except Exception as e:
+                # The exception message should be the stack trace, as returned by the LLM.
+                assert isinstance(e, Exception)
+                break
+        except Exception as e:
+            print(f"Test error: {e}. Retrying after delay...")
+            time.sleep(retry_delay)
+
+
+def test_stack_trace_with_claude():
+    retry_count = 3
+    retry_delay = 5
+
+    for _ in range(retry_count):
+        try:
+
+            @stack_trace(llm=claude)
             def funkodunko():
                 items = [1, 2, 3]
                 return items[5]
