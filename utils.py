167daa47e976a38e81ffe45a099a247e2fc57854
Justin Chen <ch3njus@gmail.com>
2023-06-02 15:27:19 -0400

    doc: added comment

--- utils.py
+++ utils.py
@@ -45,6 +45,23 @@ def extract_func_name(code: str) -> str:
         raise ValueError("No function definition found in provided code.")
 
 
+"""
+    Convert the provided text into a valid Python function name in snake_case.
+
+    This function performs the following operations:
+    1. Strips leading and trailing whitespace.
+    2. Converts all characters to lowercase.
+    3. Replaces non-alphanumeric characters with underscores.
+    4. If the result starts with a digit, prefixes it with an underscore.
+
+    Args:
+        text (str): The input string to convert into a valid Python function name.
+
+    Returns:
+        str: The input string converted into a valid Python function name in snake_case.
+"""
+
+
 def to_func_name(text: str) -> str:
     # remove leading and trailing whitespace
     text = text.strip()
