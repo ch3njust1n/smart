2325abc32e34f50e53e8b0b1510298775c4a04d6
Justin Chen <ch3njus@gmail.com>
2023-06-04 11:03:13 -0400

    chore: section for dev reqs and indicate package is type hinted

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="meta",
+    name="generative",
     version="0.1",
     packages=find_packages(),
     install_requires=[
         "anthropic>=0.2.9",
         "openai>=0.27.7",
         "python-dotenv>=1.0.0",
-        "pytest>=7.3.1",
         "RestrictedPython>=6.0",
     ],
+    extras_require={
+        "dev": [
+            "pytest>=7.3.1",
+        ]
+    },
+    package_data={"generative": ["py.typed"]},
 )
