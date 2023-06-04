76ce784ac5bf91ba269b8c4b86e9eb0db392bf57
Justin Chen <ch3njus@gmail.com>
2023-06-04 16:20:28 -0400

    refactor: use pytest.parametrize

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,30 +1,26 @@
 # Demo code for dynamic metaprogramming decorator
 import time
+import pytest
 from generative.decorators import adapt, catch, stack_trace
 from .model import gpt3, gpt4, claude
 
 
-def test_add():
-    a = """
+@pytest.mark.parametrize(
+    "code",
+    [
+        "",
+        """
     def add(a, b):
         return sum([a, b])
-    """
-
-    @adapt(a)
+    """,
+    ],
+)
+def test_add(code):
+    @adapt(code)
     def add(a, b):
         return a + b
 
     assert add(3, 4) == 7, "Test failed: Expected 7"
-
-
-def test_multiply():
-    b = ""
-
-    @adapt(b)
-    def multiply(a, b):
-        return a * b
-
-    assert multiply(3, 4) == 12, "Test failed: Expected 12"
 
 
 def test_self_healing_no_llm():
@@ -45,14 +41,15 @@ def test_self_healing_no_llm():
     assert broken_fibonacci(8) == 21
 
 
-def test_self_healing_with_gpt():
+@pytest.mark.parametrize("model,critic", [(gpt4, claude), (claude, gpt3)])
+def test_self_healing_with_llm(model, critic):
     retry_count = 3
     retry_delay = 5
 
     for _ in range(retry_count):
         try:
 
-            @adapt(model=gpt4, critic=claude)
+            @adapt(model=model, critic=critic)
             def fibonacci(n):
                 if n <= 0:
                     return 0
@@ -68,37 +65,15 @@ def test_self_healing_with_gpt():
             time.sleep(retry_delay)
 
 
-def test_self_healing_with_claude():
+@pytest.mark.parametrize("model,critic", [(gpt4, claude), (claude, gpt3)])
+def test_adapt_with_gpt(model, critic):
     retry_count = 3
     retry_delay = 5
 
     for _ in range(retry_count):
         try:
 
-            @adapt(model=claude, critic=gpt3)
-            def fibonacci(n):
-                if n <= 0:
-                    return 0
-                elif n == 1:
-                    return 1
-                else:
-                    return fibonacci(n - 1) + fibonacci(n - 2) + 1
-
-            assert fibonacci(8) == 21
-            break
-        except Exception as e:
-            print(f"Test error: {e}. Retrying after delay...")
-            time.sleep(retry_delay)
-
-
-def test_adapt_with_gpt():
-    retry_count = 3
-    retry_delay = 5
-
-    for _ in range(retry_count):
-        try:
-
-            @adapt(model=gpt4, critic=claude)
+            @adapt(model=model, critic=critic)
             def func(a, b):
                 prompt = """
                 Write a complete python 3 function, including the header and
@@ -112,35 +87,15 @@ def test_adapt_with_gpt():
             time.sleep(retry_delay)
 
 
-def test_adapt_with_claude():
-    retry_count = 3
-    retry_delay = 5
-
-    for _ in range(retry_count):
-        try:
-
-            @adapt(model=claude, critic=gpt3)
-            def func(a, b):
-                prompt = """
-                Write a complete python 3 function, including the header and
-                return statement that computes the N-th Fibonacci number.
-                """
-
-            assert func(8) == 21
-            break
-        except Exception as e:
-            print(f"Test error: {e}. Retrying after delay...")
-            time.sleep(retry_delay)
-
-
-def test_catch_with_gpt():
+@pytest.mark.parametrize("model", [gpt4, gpt3, claude])
+def test_catch_with_gpt(model):
     retry_count = 3
     retry_delay = 5
 
     for _ in range(retry_count):
         try:
             # Define a function that raises an exception
-            @catch(model=gpt4)
+            @catch(model=model)
             def func(a, b):
                 raise Exception("Original function exception")
 
@@ -153,34 +108,15 @@ def test_catch_with_gpt():
             time.sleep(retry_delay)
 
 
-def test_catch_with_claude():
-    retry_count = 3
-    retry_delay = 5
-
-    for _ in range(retry_count):
-        try:
-            # Define a function that raises an exception
-            @catch(model=claude)
-            def func(a, b):
-                raise Exception("Original function exception")
-
-            # Since the original function raises an exception, we expect the LLM
-            # to provide an implementation that returns the sum of a and b
-            assert func(3, 4) == 7
-            break
-        except Exception as e:
-            print(f"Test error: {e}. Retrying after delay...")
-            time.sleep(retry_delay)
-
-
-def test_func_stack_trace_with_gpt():
+@pytest.mark.parametrize("model", [gpt4, gpt3, claude])
+def test_func_stack_trace_with_gpt(model):
     retry_count = 5
     retry_delay = 5
 
     for _ in range(retry_count):
         try:
 
-            @stack_trace(model=gpt4)
+            @stack_trace(model=model)
             def funkodunko():
                 items = [1, 2, 3]
                 return items[5]
@@ -196,31 +132,9 @@ def test_func_stack_trace_with_gpt():
             time.sleep(retry_delay)
 
 
-def test_stack_trace_with_claude():
-    retry_count = 3
-    retry_delay = 5
-
-    for _ in range(retry_count):
-        try:
-
-            @stack_trace(model=claude)
-            def funkodunko():
-                items = [1, 2, 3]
-                return items[5]
-
-            try:
-                funkodunko()
-            except Exception as e:
-                # The exception message should be the stack trace, as returned by the LLM.
-                assert isinstance(e, Exception)
-                break
-        except Exception as e:
-            print(f"Test error: {e}. Retrying after delay...")
-            time.sleep(retry_delay)
-
-
-def test_class_stack_trace_decorator_with_gpt():
-    @stack_trace(model=gpt4)
+@pytest.mark.parametrize("model", [gpt4, gpt3, claude])
+def test_class_stack_trace_decorator_with_gpt(model):
+    @stack_trace(model=model)
     class DecoratedClass:
         def func(self):
             raise ValueError("Induced exception")
@@ -241,16 +155,9 @@ def test_class_stack_trace_decorator_with_gpt():
             time.sleep(retry_delay)
 
 
-def test_is_not_generative():
-    @adapt()
-    def func():
-        pass
-
-    assert not func._is_generative
-
-
-def test_is_generative():
-    @adapt(model=gpt3)
+@pytest.mark.parametrize("model", [claude, None])
+def test_is_generative(model):
+    @adapt(model=model)
     def func():
         pass
 
--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -5,49 +5,23 @@ from generative.utils import format_binary_output
 
 
 @pytest.mark.skip(reason="Unstable")
-def test_is_semantically_correct_with_gpt4():
+@pytest.mark.parametrize("model", [gpt4, gpt3, claude])
+def test_is_semantically_correct(model):
     code = """
     def add(a, b):
         return sum([a, b])
     """
     input = {"a": 1, "b": 2}
     prompt = format_semantic_checker(code, input, context="")
-    output = gpt4(prompt)
+    output = model(prompt)
     output = format_binary_output(output)
 
     assert output
 
 
 @pytest.mark.skip(reason="Unstable")
-def test_is_semantically_correct_with_gpt3():
-    code = """
-    def add(a, b):
-        return sum([a, b])
-    """
-    input = {"a": 1, "b": 2}
-    prompt = format_semantic_checker(code, input, context="")
-    output = gpt3(prompt)
-    output = format_binary_output(output)
-
-    assert output
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_is_semantically_correct_with_claude():
-    code = """
-    def add(a, b):
-        return sum([a, b])
-    """
-    input = {"a": 1, "b": 2}
-    prompt = format_semantic_checker(code, input, context="")
-    output = claude(prompt)
-    output = format_binary_output(output)
-
-    assert output
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_factorial_with_gpt4():
+@pytest.mark.parametrize("model", [gpt4, gpt3, claude])
+def test_incorrect_factorial(model):
     code = """
     def factorial(n):
         if n == 0:
@@ -57,50 +31,15 @@ def test_incorrect_factorial_with_gpt4():
     """
     input = "5"
     prompt = format_semantic_checker(code, input, context="")
-    output = gpt4(prompt)
+    output = model(prompt)
     output = format_binary_output(output)
 
     assert output == False
 
 
 @pytest.mark.skip(reason="Unstable")
-def test_incorrect_factorial_with_gpt3():
-    code = """
-    def factorial(n):
-        if n == 0:
-            return 1
-        else:
-            return n - factorial(n-1)
-    """
-    input = "5"
-    prompt = format_semantic_checker(code, input, context="")
-    output = gpt3(prompt)
-    output = format_binary_output(output)
-
-    assert output == False
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_factorial_with_claude():
-    code = """
-    def factorial(n):
-        if n == 0:
-            return 1
-        else:
-            return n - factorial(n-1)
-    """
-    input = "5"
-    prompt = format_semantic_checker(code, input, context="")
-    output = claude(prompt)
-    print(f"output: {output}")
-    output = format_binary_output(output)
-    print(f"formatted: {output}")
-
-    assert output == False
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_valueerror_with_gpt4():
+@pytest.mark.parametrize("model", [gpt4, gpt3, claude])
+def test_incorrect_valueerror(model):
     code = """
     try:
         x = 5 / 0
@@ -108,44 +47,15 @@ def test_incorrect_valueerror_with_gpt4():
         x = 0
     """
     prompt = format_semantic_checker(code, input, context="")
-    output = gpt4(prompt)
+    output = model(prompt)
     output = format_binary_output(output)
 
     assert output == False
 
 
 @pytest.mark.skip(reason="Unstable")
-def test_incorrect_valueerror_with_gpt3():
-    code = """
-    try:
-        x = 5 / 0
-    except ValueError:
-        x = 0
-    """
-    prompt = format_semantic_checker(code, input, context="")
-    output = gpt3(prompt)
-    output = format_binary_output(output)
-
-    assert output == False
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_valueerror_with_claude():
-    code = """
-    try:
-        x = 5 / 0
-    except ValueError:
-        x = 0
-    """
-    prompt = format_semantic_checker(code, input, context="")
-    output = claude(prompt)
-    output = format_binary_output(output)
-
-    assert output == False
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_zerodivisionerror_with_gpt4():
+@pytest.mark.parametrize("model", [gpt4, gpt3, claude])
+def test_incorrect_zerodivisionerror(model):
     code = """
     try:
         x = 5 / 0
@@ -155,83 +65,22 @@ def test_incorrect_zerodivisionerror_with_gpt4():
     """
 
     prompt = format_semantic_checker(code, input, context="")
-    output = gpt4(prompt)
+    output = model(prompt)
     output = format_binary_output(output)
 
     assert output
 
 
 @pytest.mark.skip(reason="Unstable")
-def test_incorrect_zerodivisionerror_with_gpt3():
-    code = """
-    try:
-        x = 5 / 0
-    except ZeroDivisionError:
-        x = 0
-    print(x)
-    """
-
-    prompt = format_semantic_checker(code, input, context="")
-    output = gpt3(prompt)
-    output = format_binary_output(output)
-
-    assert output
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_zerodivisionerror_with_claude():
-    code = """
-    try:
-        x = 5 / 0
-    except ZeroDivisionError:
-        x = 0
-    print(x)
-    """
-
-    prompt = format_semantic_checker(code, input, context="")
-    output = claude(prompt)
-    output = format_binary_output(output)
-
-    assert output
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_io_with_gpt4():
+@pytest.mark.parametrize("model", [gpt4, gpt3, claude])
+def test_incorrect_io_with(model):
     code = """
     with open('somefile.txt', 'r') as f:
         f.write('some text')
     """
 
     prompt = format_semantic_checker(code, input, context="")
-    output = gpt4(prompt)
-    output = format_binary_output(output)
-
-    assert output == False
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_io_with_gpt3():
-    code = """
-    with open('somefile.txt', 'r') as f:
-        f.write('some text')
-    """
-
-    prompt = format_semantic_checker(code, input, context="")
-    output = gpt3(prompt)
-    output = format_binary_output(output)
-
-    assert output == False
-
-
-@pytest.mark.skip(reason="Unstable")
-def test_incorrect_io_with_claude():
-    code = """
-    with open('somefile.txt', 'r') as f:
-        f.write('some text')
-    """
-
-    prompt = format_semantic_checker(code, input, context="")
-    output = claude(prompt)
+    output = model(prompt)
     output = format_binary_output(output)
 
     assert output == False
