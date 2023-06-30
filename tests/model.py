5885aa3e35eb230a8f3dfdb2a68485d09189d6c3
Justin Chen <ch3njus@gmail.com>
2023-06-29 21:43:34 -0400

    feat: added google palm

--- tests/model.py
+++ tests/model.py
@@ -5,6 +5,7 @@ User-defined functions that generate code using LLM.
 import os
 import openai
 import anthropic
+import google.generativeai as palm
 from dotenv import load_dotenv
 
 from generative.metaclasses import AbstractGenerativeModel
@@ -89,7 +90,14 @@ class Claude(AbstractGenerativeModel):
 
     @classmethod
     def generate(cls, prompt: str) -> str:
-        c = anthropic.Client(os.getenv("ANTHROPIC_API_KEY"))
+        api_key = os.getenv("ANTHROPIC_API_KEY")
+        c = anthropic.Client(api_key)
+
+        if api_key is None:
+            raise ValueError(
+                "The ANTHROPIC_API_KEY environment variable is not set. Please provide your ANTHROPIC API key."
+            )
+
         llm_code = c.completion(
             prompt=f"{anthropic.HUMAN_PROMPT} {prompt}{anthropic.AI_PROMPT}",
             stop_sequences=[anthropic.HUMAN_PROMPT],
@@ -99,3 +107,35 @@ class Claude(AbstractGenerativeModel):
         )
 
         return llm_code["completion"]
+
+
+class Palm(AbstractGenerativeModel):
+    """
+    PaLM API wrapper
+
+    Args:
+        prompt (string): The source code as context for function to replace.
+
+    Returns:
+        Source code of the generated function.
+    """
+
+    @classmethod
+    def generate(cls, prompt: str) -> str:
+        api_key = os.getenv("GOOGLE_API_KEY")
+        palm.configure(api_key=api_key)
+
+        if api_key is None:
+            raise ValueError(
+                "The GOOGLE_API_KEY environment variable is not set. Please provide your GOOGLE API key."
+            )
+
+        llm_code = palm.generate_text(
+            model=os.getenv("GOOGLE_MODEL"),
+            prompt=prompt,
+            temperature=float(os.getenv("TEMPERATURE", 0.7)),
+            # The maximum length of the response
+            max_output_tokens=int(os.getenv("MAX_TOKENS", 1000)),
+        )
+
+        return llm_code.result
